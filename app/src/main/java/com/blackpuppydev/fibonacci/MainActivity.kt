7d96c35939c7aaedd9308acec98f6450b4d03b8d
Nattawut.c <blackpuppy.dev@gmail.com>
2024-05-08 11:01:43 +0700

    commit

---
+++ app/src/main/java/com/blackpuppydev/fibonacci/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.blackpuppydev.fibonacci
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.list_adapter.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var itemList:ArrayList<ItemFibonacci>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        list_item.layoutManager = LinearLayoutManager(this)
+        loadView()
+
+    }
+
+    private fun loadView(){ //40index i[0]=0 i[1]=1
+
+        val index0 = ItemFibonacci()
+        index0.number = 0
+        index0.status = "test0"
+
+        val index1 = ItemFibonacci()
+        index1.number = 1
+        index1.status = "test1"
+
+        itemList = arrayListOf(index0,index1)
+
+        for (i in 2..40){
+            val index = ItemFibonacci()
+            index.number = (itemList[i-2].number + itemList[i-1].number)
+            index.status = "test$i"
+
+            Log.d("test index : " ,  "$i -> " + index.number + " , " + index.status)
+            itemList.add(index)
+        }
+
+        list_item.adapter = FibonacciAdapter()
+
+
+
+
+    }
+
+
+    inner class FibonacciAdapter: RecyclerView.Adapter<ItemViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+            return ItemViewHolder(parent)
+        }
+
+        override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+            holder.setData(itemList[position],position)
+        }
+
+        override fun getItemCount(): Int {
+            return itemList.size
+        }
+
+    }
+
+
+    inner class ItemViewHolder(parent: ViewGroup) :
+        RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_adapter, parent, false)), View.OnClickListener {
+
+//        lateinit var title:TextView
+//        lateinit var icon:ImageView
+
+
+        init {
+            list_item.setOnClickListener(this)
+        }
+
+        fun setData(item:ItemFibonacci,position: Int){
+            itemView.title.text = "Index: $position, Number: ${item.number}"
+
+        }
+
+        override fun onClick(v: View?) {
+
+            //show popup
+        }
+
+
+    }
+
+
+    class ItemFibonacci{
+        var number:Int = 0
+        var status:String = ""
+    }
+
+}
